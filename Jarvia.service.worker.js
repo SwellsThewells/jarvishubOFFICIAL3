1da65f31d9e143465fd4bf7dd5e212dd6b43d5c4
MrBrecken <Breckenvodicka@gmail.com>
2026-09-24 19:47:03 -0500

    greecd

--- Jarvia.service.worker.js
+++ Jarvia.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790294858|24541763';
+const CACHE_VERSION = '1790297172|2338723627';
 /** @type {string} */
 const CACHE_PREFIX = 'Jarvia-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
